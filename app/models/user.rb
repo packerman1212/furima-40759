bb7f54404de1c1c1991a11ae870fa6524c57903c
packerman1212 <mezase226334@gmail.com>
2024-05-11 06:25:57 +0900

    Userモデルにバリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birth_date, presence: true
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
+    # カタカナのみ許可する
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters." }
+  end
 end
