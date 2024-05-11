2a2ed33158939169063e5154a725d5a529cf59af
packerman1212 <mezase226334@gmail.com>
2024-05-12 06:35:30 +0900

    Rbocop指摘箇所の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   validates :nickname, :birth_date, presence: true
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
-    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     # カタカナのみ許可する
-    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters." }
+    validates :last_name_kana,
+              :first_name_kana,
+              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
   end
 end
