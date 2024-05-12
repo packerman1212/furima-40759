1d381d1bcd9940a2a74800ddd97abb4794ee21a6
packerman1212 <mezase226334@gmail.com>
2024-05-12 09:38:22 +0900

    passwordのバリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :birth_date, presence: true
   with_options presence: true do
+    validates :nickname, :birth_date
+    # 半角英数字混入のみ許可する
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers.'}
     # ひらがな、カタカナ、漢字のみ許可する
-    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :last_name,
+              :first_name,
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     # カタカナのみ許可する
     validates :last_name_kana,
               :first_name_kana,
