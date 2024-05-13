e49e028643cd780ec876cb2c03f59f2e0bac9299
packerman1212 <mezase226334@gmail.com>
2024-05-13 13:02:05 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,19 @@ class Item < ApplicationRecord
   belongs_to :fee_responsibility
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :name, :description
+    validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+              format: { with: /\A\d+\z/, message: 'is not a number' }
+  end
+
+  # ActiveHash attributes validation
+  validates :category_id,
+            :product_state_id,
+            :fee_responsibility_id,
+            :prefecture_id,
+            :shipping_day_id,
+            numericality: { other_than: 0, message: "can't be blank" }
 end
