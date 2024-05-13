f688c0a0b5d3284e6dbb609821a5c152f8542d06
packerman1212 <mezase226334@gmail.com>
2024-05-13 17:03:25 +0900

    JavaScriptで販売価格の計算部分を実装

---
+++ app/javascript/price_calculation.js
@@ -0,0 +1,21 @@
+function priceCalculation (){
+  const priceInput = document.getElementById('item-price')
+  // 入力イベント
+  priceInput.addEventListener('input', () => {
+    const priceValue = priceInput.value
+
+    // 手数料計算
+    const tax = Math.floor(priceValue * 0.1)
+    // 利益計算
+    const profit = Math.floor(priceValue - tax)
+
+    // 結果を表示
+    const addTaxPriceElement = document.getElementById('add-tax-price')
+    addTaxPriceElement.textContent = tax
+
+    const profitElement = document.getElementById('profit')
+    profitElement.textContent = profit;
+  })
+  }
+
+window.addEventListener('turbo:load', priceCalculation)
